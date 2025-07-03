80224a4fc756e8bd2f11cf2f1f43e038c6fa9a16
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-07-03 09:06:37 -0300

    correcao para instanciar o askme handler

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, request, Response, jsonify
-from handlers.ask_handler import ask_me_handler
+from handlers.ask_handler import AskMeHandler
 from api_manager.my_response import MyResponse
 
 app = Flask(__name__)
@@ -13,7 +13,8 @@ def hello_world():
 
 @app.route('/api/v1/askme', methods=['POST'])
 def ask_me():
-    response: MyResponse = ask_me_handler(request)
+    ask_me_handler = AskMeHandler()
+    response: MyResponse = ask_me_handler.ask_me_handler(request)
     #return Response(response.message),200
     return jsonify(response.to_dict()), response.code
 
