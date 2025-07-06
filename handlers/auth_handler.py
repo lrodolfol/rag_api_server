fc4a2d4a4d5daaca633ef1595cfa8538d266a382
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2025-07-06 18:12:11 -0300

    remove try catch

--- handlers/auth_handler.py
+++ handlers/auth_handler.py
@@ -31,43 +31,34 @@ class AuthHandler:
 
     def auth(self, request):
         self.logger.info("AuthHandler: auth method called")
-        try:
-            code = request.json.get('code')
-            if not code:
-                self.logger.error("AuthHandler: 'code' not found in request")
-                return MyResponse(400, "error: Missing code in request")
 
-            self.logger.info(f"AuthHandler: Received code: {code}")
+        code = request.json.get('code')
+        if not code:
+            self.logger.error("AuthHandler: 'code' not found in request")
+            return MyResponse(400, "error: Missing code in request")
 
-            if not self.validate_code(code):
-                return MyResponse(400, "error: Invalid code")
+        self.logger.info(f"AuthHandler: Received code: {code}")
 
-            token: str = generate_token(code)
-            return MyResponse(200, token)
+        if not self.validate_code(code):
+            return MyResponse(400, "error: Invalid code")
 
-        except Exception as e:
-            self.logger.error(f"Error in auth method: {str(e)}")
-            return MyResponse(400, "error: Verifique os dados enviados")
+        token: str = generate_token(code)
+        return MyResponse(200, token)
 
 
     def validate_code(self, code) -> bool:
-        try:
-            self.logger.info("AuthHandler: reading users-coded.txt")
-            with open(f"./files_source/users-coded.txt", 'r', encoding='utf-8') as file:
-                lines = [linha.strip() for linha in file]
+        self.logger.info("AuthHandler: reading users-coded.txt")
+        with open(f"./files_source/users-coded.txt", 'r', encoding='utf-8') as file:
+            lines = [linha.strip() for linha in file]
 
-            if code in lines:
-                self.logger.info("AuthHandler: code found in users-coded.txt")
+        if code in lines:
+            self.logger.info("AuthHandler: code found in users-coded.txt")
 
-                lines.remove(code)
-                with open(f"./files_source/users-coded.txt", 'w', encoding='utf-8') as file:
-                    for line in lines:
-                        file.write(f"{line}\n")
+            lines.remove(code)
+            with open(f"./files_source/users-coded.txt", 'w', encoding='utf-8') as file:
+                for line in lines:
+                    file.write(f"{line}\n")
 
-                return True
-            else:
-                return False
-
-        except Exception as e:
-            self.logger.error(f"Error in validate_code method: {str(e)}")
+            return True
+        else:
             return False
